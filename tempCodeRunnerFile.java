ecd8eeb09611e2f3669629606b7db3eafd4d3752
varghese25 <vbpoulose@gmail.com>
2025-03-12 12:57:14 -0400

    Java 1

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,9 @@
+public class testFive {
+public static void main(String args[]) { 
+ try { 
+ throw 10; 
+ } catch(int e) { 
+ System.out.println("Catch the exception" + e); 
+ } 
+ } 
+}
---
+++ testTwo.java
@@ -0,0 +1,11 @@
+public class testTwo {
+     public static void main(String args[]) { 
+ for(int i = 0; i < 5; i++) { 
+ if(i == 3) { 
+ break; 
+ } 
+ System.out.print(i + " "); 
+ } 
+ } 
+    
+}
