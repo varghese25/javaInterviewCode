fb42d5fc5e9edc2ac2a6697c372fc73683c24bbd
varghese25 <vbpoulose@gmail.com>
2025-04-04 20:08:20 -0400

    Java 8 math.min(int a, int b) argument

---
+++ test57.java
@@ -0,0 +1,9 @@
+
+public class test57 {
+    public static void main(String[] args) {
+        System.out.println(Math.min(10));
+    }
+
+}
+// Error: compilation error, Because Math.min() method requires two arguments,
+// Like Math.min(10, 20);
