966694f004ea62df94a776c4c854fb78464cd127
varghese25 <vbpoulose@gmail.com>
2025-04-11 13:43:42 -0400

    Java_JS_13_updated_shiv10

---
+++ shiv/shiv10.js
@@ -0,0 +1,14 @@
+// let a = 10;
+// let b = (a++, a + 5); //
+// console.log(a, b);
+
+
+// const arr =[1,2,3]
+// arr[10] = 4;
+// console.log(arr.length);
+// console.log(arr); // Output: 11 be
+
+
+console.log(typeof NaN); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
