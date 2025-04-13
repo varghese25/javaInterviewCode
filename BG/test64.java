076067850baa9688205b85dd8db8f0cdb1d2e226
varghese25 <vbpoulose@gmail.com>
2025-04-13 19:58:12 -0400

    Java_JS_15

---
+++ BG/test64.java
@@ -0,0 +1,10 @@
+public class test64 {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 4) {
+                break; // break statement will exit the loop when i is 4
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
