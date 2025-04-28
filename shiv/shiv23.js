1d652fc1fe5685949e36965ab7f209f985013fcf
varghese25 <vbpoulose@gmail.com>
2025-04-28 19:39:07 -0400

    JS_17_Code

---
+++ shiv/shiv23.js
@@ -0,0 +1,14 @@
+let data = [10];
+data[5] = 50;
+console.log(data.length);
+
+/*
+Index: 0 -> 10
+Index: 1 -> undefined
+Index: 2 -> undefined
+Index: 3 -> undefined
+Index: 4 -> undefined
+Index: 5 -> 50 
+
+
+OutPut is 6*/
