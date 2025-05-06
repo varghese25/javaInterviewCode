aba77a293e030c0da7b6b1578792d4274ffa18bd
varghese25 <vbpoulose@gmail.com>
2025-05-06 14:24:58 -0400

    Java_20

---
+++ BG/test84.java
@@ -0,0 +1,24 @@
+class Vaseekaran {
+    Vaseekaran() {
+        System.out.println("Vaseekaran is born");
+    }
+}
+
+class Chitti extends Vaseekaran {
+    Chitti() {
+        System.out.println("Chitti is activated");
+    }
+
+}
+
+    class 2 Point0 extends Chitti{2Point0() {
+ System.out.println("2.0 is unleashed");
+ }
+}
+
+public class test84 {
+    public static void main(String[] args) {
+ 2Point0 robot = new 2Point0();
+ }
+    
+}
