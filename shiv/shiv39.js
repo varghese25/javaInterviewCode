d637ea5ce40974b48f35a3093122a1dfb0d00817
varghese25 <vbpoulose@gmail.com>
2025-05-23 14:25:13 -0400

    33_JS_includesMethod

---
+++ shiv/shiv39.js
@@ -0,0 +1,7 @@
+const str = "JavaScript is powerfull";
+const result = str.includes("Script");
+console.log(result);
+/*
+
+B) true
+.includes("Script") checks if "Script" appears anywhere in "JavaScript is powerfull". Since it does, the Output is: console.log(result); // true*/
