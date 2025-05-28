af410c2b3237396657144220cc11ca560eacbe72
varghese25 <vbpoulose@gmail.com>
2025-05-28 14:06:17 -0400

    37_JavaScript_destructuring_All_Prac

--- JsCodeSimplified/Destructuring.js
+++ JsCodeSimplified/Destructuring.js
@@ -4,11 +4,11 @@ const person = {
   age: 50,
 };
 // Destructuring
-let { firstName: name, lastName, country = "India " } = person; // firstName i used Alias as name. console,log(name) will print firstName value
+//let { firstName: name, lastName, country = "India " } = person; // firstName i used Alias as name. console,log(name) will print firstName value
 // console.log(firstName);
 // console.log(age);
-console.log(country);
-console.log(name);
+// console.log(country);
+// console.log(name);
 
 // String Destructuring
 // Create a String
@@ -25,6 +25,47 @@ console.log(fruit2);
 
 // Skipping Array Values
 // Create an Array
+// const fruito = ["Bananas", "Oranges", "Apples", "Mangos"];
+// // Destructuring
+// let [fruito1, , , fruito2] = fruito;
+
+//Array Position Values
+// We can pick up values from specific index locations of an array:
+
 const fruito = ["Bananas", "Oranges", "Apples", "Mangos"];
 // Destructuring
-let [fruito1, , , fruito2] = fruito;
+let { [0]: fruito1, [1]: fruito2 } = fruito;
+console.log("Array Postion Values One is:" + fruito2);
+
+// The Rest Property
+// You can end a destructuring syntax with a rest property.
+// This syntax will store all remaining values into a new array:
+// Create an Array
+const numbers = [10, 20, 30, 40, 50, 60, 70];
+// Destructuring
+const [a, b, ...rest] = numbers;
+console.log(a, b, rest);
+
+//Destructuring Maps
+// Create a Map
+const fruitss = new Map([
+  ["Bananas", 100],
+  ["Oranges", 200],
+  ["Apples", 300],
+  ["Mangos", 400],
+]);
+
+// Destructuring
+let text = " ";
+for (let [key, value] of fruitss) {
+  text += key + " = " + value + " ";
+}
+console.log(text);
+// Destructuring with forEach
+
+//Swapping JavaScript Variables
+//You can swap the values of two variables using a destructuring assignment:
+let firstName = "Varghese ";
+let lastName = "Baby ";
+[firstName, lastName] = [lastName, firstName];
+console.log(firstName, lastName);
