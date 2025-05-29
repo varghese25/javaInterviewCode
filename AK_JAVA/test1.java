7f52882a2549d6d111e9dd92817a20b8c5f96039
varghese25 <vbpoulose@gmail.com>
2025-05-29 13:18:37 -0400

    38_Java_ASCII

---
+++ AK_JAVA/test1.java
@@ -0,0 +1,8 @@
+class test1 {
+    public static void main(String[] args) {
+        char a = 'a'; // a 97 lowercase a; ASCII Character Set
+        short b = 95;
+        System.out.println(a - b);
+
+    }
+}
