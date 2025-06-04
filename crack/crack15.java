2f8a3652715c918a76f94a8a1adbf0e762c293b4
varghese25 <vbpoulose@gmail.com>
2025-06-04 13:33:53 -0400

    43_Java_ArrayList_WithWildCard

---
+++ crack/crack15.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class crack15 {
+    public static void main(String[] args) {
+        List<?> list = new ArrayList<String>();
+        list.add(null);
+    }
+
+}   
