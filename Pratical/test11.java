bf004b65fab9a0f043817ea56414f91ca28bce13
Varghese Baby <Vbpoulose@gmail.com>
2025-08-05 12:45:08 -0400

    Java_arraySort

---
+++ Pratical/test11.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class test11 {
+
+    public static void main(String[] var0) {
+        int[] numbers = { 3, 2, 1 };
+        Arrays.sort(numbers);
+        System.out.println(numbers[0]);
+    }
+}
