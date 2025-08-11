7861a91e29430ae028e46982789a645adadc008d
Varghese Baby <Vbpoulose@gmail.com>
2025-08-11 18:19:20 -0400

    Ft:JavaWorksFlowChart

--- Pratical/test11.java
+++ Pratical/test11.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 public class test11 {
 
     public static void main(String[] var0) {
-        int[] numbers = { 3, 2, 1 };
-        Arrays.sort(numbers);
-        System.out.println(numbers[0]);
+        int[] numbers = { 3, 2, 1 }; // Initialize an array with three integers
+        Arrays.sort(numbers); // Sort the array in ascending order {1,2,3}
+        System.out.println(numbers[0]); // Print the first element of the sorted array Answers: 1
     }
 }
