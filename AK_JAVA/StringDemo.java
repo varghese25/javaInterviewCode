a2e9edcc6484863b0613c52a8d0cffb5202b13ca
Varghese Baby <Vbpoulose@gmail.com>
2025-08-12 18:52:29 -0400

    Java_String_heapAndPoolMemory

---
+++ AK_JAVA/StringDemo.java
@@ -0,0 +1,12 @@
+class StringDemo {
+    public static void main(String[] args) {
+        String name1 = new String("arbaz khan");
+        String name2 = "arbaz khan";
+
+        if (name1 == name2) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
