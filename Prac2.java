c89cec3429ba7822d7602d8d4fcdfea6a1a1c67c
Varghese Baby <Vbpoulose@gmail.com>
2025-09-03 18:43:22 -0400

    Java: Plus-Operator

---
+++ Prac2.java
@@ -0,0 +1,9 @@
+public class Prac2 {
+    public static void main(String[] args) {
+
+        System.out.println("A" + "B"); // String concatenation, so "A" + "B" = "AB"
+        System.out.println('A' + 'B'); // 'A' is 65 and 'B' is 66 in ASCII table, so 65 + 66 = 131
+
+    }
+
+}
